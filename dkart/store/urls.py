a416a66ee9eaca33b679e05f106d82cc7f7a74ff
Dhyey <dhyeykakadiya1015@gmail.com>
2024-07-25 17:46:25 +0530

    pushed because an error occurred

--- dkart/store/urls.py
+++ dkart/store/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.store, name='store')
+    path('', views.store, name='store'),
+    path('<slug: category_slug>', views.store, name = 'product_by_category'),
+    
 ]
