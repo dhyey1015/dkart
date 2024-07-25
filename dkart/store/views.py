1a031f6f8a36f6fed5aed39d6006b813be34b383
Dhyey <dhyeykakadiya1015@gmail.com>
2024-07-25 13:31:36 +0530

    rendered all the available product dynamically in the page store

--- dkart/store/views.py
+++ dkart/store/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def store(request):
-    return render(request,'store/store.html')
+    product = Product.objects.all().filter(is_available = True)
+    
+    context = {
+        'products':product,
+    }
+
+    
+    return render(request, 'store/store.html')
     
