bbf3a54077836baedb794e9566b93c20c2ed2d9a
Dhyey <dhyeykakadiya1015@gmail.com>
2024-07-25 13:58:09 +0530

    added product count variable to count product

--- dkart/store/views.py
+++ dkart/store/views.py
@@ -4,6 +4,7 @@ from .models import Product
 # Create your views here.
 def store(request):
     product = Product.objects.all().filter(is_available = True)
+    product_count = product.count()
     
     context = {
         'products':product,
