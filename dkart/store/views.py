1288e304b7be5417e4a05e6a925d42f02805dfcd
Dhyey <dhyeykakadiya1015@gmail.com>
2024-08-01 21:24:24 +0530

    added a search function which operated search part of the site

--- dkart/store/views.py
+++ dkart/store/views.py
@@ -4,6 +4,8 @@ from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage,PageNotAnInteger, Paginator
+from django.http import HttpResponse
+from django.db.models import Q
 # Create your views here.
 def store(request, category_slug= None):
     categories = None
@@ -45,3 +47,19 @@ def product_detail(request,category_slug,product_slug):
     }
           
     return render(request, 'store/product_detail.html',context)
+
+def search(request):
+    products = None
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(
+                Q(description__icontains= keyword) | Q(product_name__icontains = keyword)
+                )
+            product_count = products.count()
+            
+    context = {
+        'products':products,
+        'product_count':product_count
+    }
+    return render(request, 'store/store.html', context)
