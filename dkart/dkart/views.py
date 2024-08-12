6945a41e897b0ec0eb03f7698026213caf62768e
Dhyey <dhyeykakadiya1015@gmail.com>
2024-08-12 19:39:42 +0530

    section 25 complete

--- dkart/dkart/views.py
+++ dkart/dkart/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render 
 from django.http import HttpResponse
-from store.models import Product
+from store.models import Product,ReviewRating
 
 def home(request):
-    product = Product.objects.all().filter(is_available = True)
+    product = Product.objects.all().filter(is_available = True).order_by('created_date')
+    
+    #get the reviews
+    for product1 in product:
+        reviews = ReviewRating.objects.filter(product_id = product1.id, status = True)
     
     context = {
         'products':product,
+        'reviews': reviews,
     }
 
     return render(request,'home.html',context)
--- dkart/store/views.py
+++ dkart/store/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Product
-from category.models import Category
+from .models import Product, ProductGallery, ReviewRating
+from category.models import Category    
 from carts.models import CartItem
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage,PageNotAnInteger, Paginator
 from django.http import HttpResponse
 from django.db.models import Q
-from .models import ReviewRating
 from .forms import ReviewForm
 from django.contrib import  messages
 from orders.models import OrderProduct
@@ -57,14 +56,15 @@ def product_detail(request,category_slug,product_slug):
     #get the reviews
     reviews = ReviewRating.objects.filter(product_id = single_product.id, status = True)
     
-        
-          
+    #get the product gallary
+    product_gallery = ProductGallery.objects.filter(product_id = single_product.id)
     
     context = {
         'single_product': single_product,
         'in_cart':in_cart,
         'orderproduct': orderproduct,
         'reviews': reviews,
+        'product_gallery': product_gallery,
     }
           
     return render(request, 'store/product_detail.html',context)
