3707503b1281ac7d8bd9f0024d8e9a0b087da2c7
Dhyey <dhyeykakadiya1015@gmail.com>
2024-08-20 23:05:56 +0530

    superuser got created

--- dkart/dkart/settings.py
+++ dkart/dkart/settings.py
@@ -116,6 +116,9 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+    
+    
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
