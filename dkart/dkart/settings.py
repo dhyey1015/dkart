cf41cf8e86f08a98ec82bac09421948d1fdbabf6
Dhyey <dhyeykakadiya1015@gmail.com>
2024-08-20 20:47:02 +0530

    made static/ to /static/

--- dkart/dkart/settings.py
+++ dkart/dkart/settings.py
@@ -150,10 +150,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/'static'
 STATICFILES_DIRS= [
-   'dkart/static',  
+   BASE_DIR/'dkart/static',  
 ]
 
 
