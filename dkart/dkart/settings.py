e453c95fb643cdfa2f6b4d214908d95f843a4c19
Dhyey <dhyeykakadiya1015@gmail.com>
2024-08-21 20:32:52 +0530

    pip freeze

--- dkart/dkart/settings.py
+++ dkart/dkart/settings.py
@@ -165,27 +165,27 @@ STATICFILES_DIRS= [
 # # AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 
 
-# AWS CONFIGURATION
-AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+# # AWS CONFIGURATION
+# AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
 
-#STORAGES settings 
-#BASIC STORAGE CONFIGURATIONS FOR S3
-AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
+# #STORAGES settings 
+# #BASIC STORAGE CONFIGURATIONS FOR S3
+# AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
 
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-AWS_S3_FILE_OVERWRITE = False
-STORAGES = {
-    # Media files
-    "default": {
-        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
-    },
+# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+# AWS_S3_FILE_OVERWRITE = False
+# STORAGES = {
+#     # Media files
+#     "default": {
+#         "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+#     },
 
-    "staticfiles": {
-        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
-    },
-}
+#     "staticfiles": {
+#         "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+#     },
+# }
 
 
 
