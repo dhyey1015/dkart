145e5b737ed3eb4e322a4dbde0f6e455fe8b95a8
Dhyey <dhyeykakadiya1015@gmail.com>
2024-08-22 18:02:20 +0530

    trying to solve static file not showing error

--- dkart/dkart/settings.py
+++ dkart/dkart/settings.py
@@ -153,11 +153,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
-STATICFILES_DIRS = [
-    BASE_DIR/'dkart/static',
-]
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR /'static'
+# STATICFILES_DIRS = [
+#     BASE_DIR/'dkart/static',
+# ]
 
 
 
@@ -169,14 +169,14 @@ STATICFILES_DIRS = [
 # AWS CONFIGURATION
 AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
-
-#STORAGES settings 
-#BASIC STORAGE CONFIGURATIONS FOR S3
 AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
-
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 AWS_S3_FILE_OVERWRITE = False
+
+STATICFILES_DIRS = [
+    BASE_DIR/'dkart/static',
+]
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 STORAGES = {
     # Media files
     "default": {
@@ -187,7 +187,6 @@ STORAGES = {
         "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
     },
 }
-
 
 
 
@@ -209,7 +208,7 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast = bool)
 
-
+    
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
