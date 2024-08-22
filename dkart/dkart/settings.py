825de06199cc62c4d29345b7cb074b80553bbaa7
Dhyey <dhyeykakadiya1015@gmail.com>
2024-08-22 17:40:20 +0530

    made some change in staticfiles_dirs

--- dkart/dkart/settings.py
+++ dkart/dkart/settings.py
@@ -153,11 +153,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-# STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR /'static'
-# STATICFILES_DIRS= [
-#     BASE_DIR /'dkart/static',  
-# ]
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    BASE_DIR/'dkart/static',
+]
+
 
 
 #AWS S3 Static Files Configuration
